8609cd3f0ab24a87ffa61cb7ed2dc9edf5a6a80d
Udit Karode <udit.karode@gmail.com>
2020-10-12 17:20:29 +0530

    temp: replace incorrect cmake version

--- temp.sh
+++ temp.sh
@@ -1,4 +1,4 @@
-/opt/android-sdk/cmdline-tools/tools/bin/sdkmanager --install "ndk;21.0.6113669" "cmake;21.0.6113669" || true
+/opt/android-sdk/cmdline-tools/tools/bin/sdkmanager --install "ndk;21.0.6113669" "cmake;3.10.2.4988404" || true
 cd /tmp
 git clone https://github.com/uditkarode/AbleMusicPlayer
 cd AbleMusicPlayer
